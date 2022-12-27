ff3db5a82e60729120ea5b8f1990a65bda880d27
Mannan Shihab <amannanshihab@gmail.com>
2022-12-27 16:20:51 +0600

    update

---
+++ src/views/NewView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="new">
+      <h1>This is an new page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .new {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
